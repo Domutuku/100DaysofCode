044f30ea655fdc412c088d951ebe57616ddf8c13
Dolores Mutuku <androgymo@gmail.com>
2022-06-10 13:30:34 -0400

    cleaner version

--- RockPaperScissors_v2.py
+++ RockPaperScissors_v2.py
@@ -29,3 +29,30 @@ scissors = '''
 '''
 
 #Write your code below this line 👇
+
+options = [rock, paper, scissors]
+
+
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors."))
+computer_choice = random.randint(0,2)
+
+if user_choice == 0 and computer_choice == 2:
+    print(options[user_choice])
+    print(options[computer_choice])
+    print("You win.")
+elif computer_choice == 0 and user_choice == 2:
+    print(options[user_choice])
+    print(options[computer_choice])
+    print("You lose.")
+elif computer_choice > user_choice:
+    print(options[user_choice])
+    print(options[computer_choice])
+    print("You lose.")
+elif user_choice > computer_choice:
+    print(options[user_choice])
+    print(options[computer_choice])
+    print("You win.")
+elif user_choice == computer_choice:
+    print(options[user_choice])
+    print(options[computer_choice])
+    print("It's a draw.")
